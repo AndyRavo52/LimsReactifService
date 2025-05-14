e9d74155ef5be60b813ed87adf2f1bff50394835
Mi-aro-101 <miaroramanandraitsiory@gmail.com>
2025-05-14 19:47:16 +0300

    Fix etat de stock en temps réel

--- Dtos/ResteStockDto.cs
+++ Dtos/ResteStockDto.cs
@@ -4,9 +4,7 @@ namespace LimsReactifService.Dtos;
 
 public class ResteStockDto
 {
-    [Column("result")]
     public double Quantite { get; set; }
-    [Column("unite")]
     public string Unite { get; set; } = string.Empty;
     public DateTime DateParam { get; set; } = DateTime.Now;
     public int IdReactif { get; set; }
